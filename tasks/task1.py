13935d21df4a137e4d07a7e27d784e8d182c6abd
nstssss <nastastrumanowa@gmail.com>
2025-11-14 23:20:36 +0300

    задание 1

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,6 +2,22 @@ import concurrent.futures
 STEP = 0.01
 
 class Task1:
+    def __init__(self, start, end, n):
+        self.start = start
+        self.end = end
+        self.n = n
+
+    def generator(self):
+        x = self.start
+        while x <= self.end:
+            yield 0.5 * x - 2
+            x += STEP
+
+    def get_values(self, n : int):
+        g = self.generator()
+        return [next(g) for _ in range(n)]
+
+class Task1_parall:
     def __init__(self, start: int, end: int):
         self.start = start
         self.end = end
@@ -18,11 +34,11 @@ class Task1:
         return numbers
 
     def parallel_execution(self, n):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
             future = executor.submit(self.get_values, n)
             return future.result()
 
-task1 = Task1(1, 101)
+task1 = Task1_parall(1, 101)
 res = task1.parallel_execution(50)
 cnt = 1
 for r in res:
