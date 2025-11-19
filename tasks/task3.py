3a7a002b4e34ee14df99a03729b8d300cb9f2d7a
nstssss <nastastrumanowa@gmail.com>
2025-11-20 01:42:29 +0300

    добавление конструктора в task3

--- tasks/task3.py
+++ tasks/task3.py
@@ -1,19 +1,23 @@
 import concurrent.futures
 
 class Task3:
-    def write_max(self, _list):
-        numbers = set(list(map(int, _list.split())))
+    def __init__(self, _list):
+        self.list = _list
+    def write_max(self):
+        numbers = set(list(map(int, self.list.split())))
         sorted_numbers = sorted(numbers, reverse=True)
         return sorted_numbers[:4]
 # task3 = Task3()
 # res = task3.write_max("4 6 2 7 9 1 1")
 # print(res)
 class Task3_parall:
+    def __init__(self, _list):
+        self.list = _list
     def process_number(self, num_str):
         return int(num_str.strip())
 
-    def parallel_execution(self, _list):
-        numbers_list = _list.split()
+    def parallel_execution(self):
+        numbers_list = self.list.split()
         with concurrent.futures.ThreadPoolExecutor() as executor:
             numbers = list(executor.map(self.process_number, numbers_list))
 
@@ -21,6 +25,6 @@ class Task3_parall:
         sorted_numbers = sorted(unique_numbers, reverse=True)
         return sorted_numbers[:4]
 
-# task3_p = Task3_parall()
-# res = task3_p.parallel_execution(" 54 2 256 7 43  13456 23245")
+# task3_p = Task3_parall(" 54 2 256 7 43  13456 23245")
+# res = task3_p.parallel_execution()
 # print(res)
